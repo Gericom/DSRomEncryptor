5f1c1e889ae2dbabd5230232194c01bd98b64de9
Emma Eilefsen Glenna <emma@eilefsen.no>
2026-08-06 20:10:26 +0200

    Add fallback for blowfish in working directory

--- DSRomEncryptor/BlowfishLocator.cs
+++ DSRomEncryptor/BlowfishLocator.cs
@@ -21,7 +21,7 @@ sealed class BlowfishLocator
         ntrBlowfish = null;
 
         // Try to get directly from a file
-        if (TryLoadFileFromApplicationFolder(NTR_BLOWFISH_NAME, out var blowfish) &&
+        if (TryLoadFile(NTR_BLOWFISH_NAME, out var blowfish) &&
             blowfish.Length == Blowfish.KEY_TABLE_LENGTH)
         {
             ntrBlowfish = blowfish;
@@ -29,7 +29,7 @@ sealed class BlowfishLocator
         }
 
         // Try to get from a DS arm7 bios dump
-        if (!success && TryLoadFileFromApplicationFolder(BIOS_NDS_7_NAME, out var biosNds7) &&
+        if (!success && TryLoadFile(BIOS_NDS_7_NAME, out var biosNds7) &&
             biosNds7.Length == BIOS_NDS_7_LENGTH)
         {
             ntrBlowfish = biosNds7[BIOS_NDS_7_BLOWFISH_OFFSET..(BIOS_NDS_7_BLOWFISH_OFFSET + Blowfish.KEY_TABLE_LENGTH)];
@@ -45,7 +45,7 @@ sealed class BlowfishLocator
         twlBlowfish = null;
 
         // Try to get directly from a file
-        if (TryLoadFileFromApplicationFolder(TWL_BLOWFISH_NAME, out var blowfish)
+        if (TryLoadFile(TWL_BLOWFISH_NAME, out var blowfish)
             && blowfish.Length == Blowfish.KEY_TABLE_LENGTH)
         {
             twlBlowfish = blowfish;
@@ -53,7 +53,7 @@ sealed class BlowfishLocator
         }
 
         // Try to get from a DSi arm7 bios dump
-        if (!success && TryLoadFileFromApplicationFolder(BIOS_DSI_7_NAME, out var biosDsi7) &&
+        if (!success && TryLoadFile(BIOS_DSI_7_NAME, out var biosDsi7) &&
             biosDsi7.Length == BIOS_DSI_7_LENGTH)
         {
             twlBlowfish = biosDsi7[BIOS_DSI_7_BLOWFISH_OFFSET..(BIOS_DSI_7_BLOWFISH_OFFSET + Blowfish.KEY_TABLE_LENGTH)];
@@ -66,8 +66,19 @@ sealed class BlowfishLocator
     public bool TryGetTwlDevBlowfish([NotNullWhen(returnValue: true)] out byte[]? twlDevBlowfish)
     {
         // Try to get directly from a file
-        return TryLoadFileFromApplicationFolder(TWL_DEV_BLOWFISH_NAME, out twlDevBlowfish);
+        return TryLoadFile(TWL_DEV_BLOWFISH_NAME, out twlDevBlowfish);
     }
+
+    private bool TryLoadFile(string fileName, [NotNullWhen(returnValue: true)] out byte[]? fileData)
+    {
+		if (TryLoadFileFromWorkingFolder(fileName, out fileData)) {
+			return true;
+		}
+		if (TryLoadFileFromApplicationFolder(fileName, out fileData)) {
+			return true;
+		}
+		return false;
+	}
 
     private bool TryLoadFileFromApplicationFolder(string fileName, [NotNullWhen(returnValue: true)] out byte[]? fileData)
     {
@@ -75,6 +86,21 @@ sealed class BlowfishLocator
         try
         {
             fileData = File.ReadAllBytes(Path.Combine(appDirectory, fileName));
+            return true;
+        }
+        catch
+        {
+            fileData = null;
+            return false;
+        }
+    }
+
+    private bool TryLoadFileFromWorkingFolder(string fileName, [NotNullWhen(returnValue: true)] out byte[]? fileData)
+    {
+        string dir = Directory.GetCurrentDirectory();
+        try
+        {
+            fileData = File.ReadAllBytes(Path.Combine(dir, fileName));
             return true;
         }
         catch
